a9649645c157a245ece21abe0945e3c114015d45
Mathew Kostiuk <mathewkostiuk@gmail.com>
2020-10-26 13:00:19 +0900

    Initial repo commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Translator
+
+This is a Shopify app that connects merchant's stores to the DeepL language translation API.
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Language translations provided by DeepL! 🎉</Heading>
+  </Page>
+);
+
+export default Index;
